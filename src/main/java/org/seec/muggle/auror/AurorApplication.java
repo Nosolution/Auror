4484ea4b1edbba141ac5f3eb1c83fd8225d0ff82
Hufflepuff <32201220+Nosolution@users.noreply.github.com>
2019-03-30 20:40:22 +0800

    init project

---
+++ src/main/java/org/seec/muggle/auror/AurorApplication.java
@@ -0,0 +1,13 @@
+package org.seec.muggle.auror;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AurorApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AurorApplication.class, args);
+    }
+
+}
